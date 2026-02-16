8d4e8f0a27e14d7fa4e0882a400ed3bada979e6a
geobour <bourantasgeorge87@gmail.com>
2026-02-16 20:27:33 +0200

    Add h1 wrapper in UsersTable

--- src/UsersTable.jsx
+++ src/UsersTable.jsx
@@ -1,24 +1,27 @@
 export default function UsersTable({ users }) {
   return (
-    <table border="1" cellPadding="10" cellSpacing="0">
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Role</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((u) => (
-          <tr key={u.id}>
-            <td>{u.id}</td>
-            <td>{u.name}</td>
-            <td>{u.email}</td>
-            <td>{u.role}</td>
+    <div>
+      <h1></h1>
+      <table border="1" cellPadding="10" cellSpacing="0">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {users.map((u) => (
+            <tr key={u.id}>
+              <td>{u.id}</td>
+              <td>{u.name}</td>
+              <td>{u.email}</td>
+              <td>{u.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
